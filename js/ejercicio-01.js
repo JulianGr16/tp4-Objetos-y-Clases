534130039ddabb87c3faf2c6691e6c5358dcd0aa
JulianGaray16 <juliangaray.2116@gmail.com>
2024-07-29 20:24:06 -0300

    se realizo el ejercicio-01 correctamente

---
+++ js/ejercicio-01.js
@@ -0,0 +1,15 @@
+const auto = {
+    color: "Negro",
+    marca: "Toyota",
+    modelo: "Corolla Cross",
+
+    encender: () =>{
+        document.write('<p>✅El auto esta encendido</p>')
+    },
+    apagar: () =>{
+        document.write('<p>⛔El auto esta apagado</p>')
+    }
+}
+
+auto.encender();
+auto.apagar();
